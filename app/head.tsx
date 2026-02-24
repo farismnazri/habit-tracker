0ea331f3a1cf73b9d11c854c284eccd5f97a42c2
Faris Nazri <faris.mnazri@gmail.com>
2026-02-24 18:17:50 +0800

    Add legacy iOS touch icon tags

--- app/head.tsx
+++ app/head.tsx
@@ -1,10 +1,12 @@
 export default function Head() {
   return (
     <>
-      {/* Explicit iOS links: Safari home-screen icon selection can ignore metadata in some builds/caches. */}
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-v3.png" />
-      <link rel="apple-touch-icon" sizes="167x167" href="/apple-touch-icon-v3-167x167.png" />
-      <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-v3-152x152.png" />
+      {/* iOS Safari can be stubborn here: include explicit legacy + sized links. */}
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=4" />
+      <link rel="apple-touch-icon-precomposed" href="/apple-touch-icon.png?v=4" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-v3.png?v=4" />
+      <link rel="apple-touch-icon" sizes="167x167" href="/apple-touch-icon-v3-167x167.png?v=4" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-v3-152x152.png?v=4" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-title" content="Habits" />
     </>
